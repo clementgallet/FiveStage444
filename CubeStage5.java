77805c08d9d1afdc4dcf9037b947ba2aabd01aa8
Clément Gallet <clement.gallet@ens-lyon.org>
2012-07-11 18:33:16 +0200

    Optimize dist stage5

--- CubeStage5.java
+++ CubeStage5.java
@@ -40,24 +40,28 @@ public final class CubeStage5 {
 	}
 
 	public int getDistanceEdgCen (){
-		CubeStage5 cube1 = new CubeStage5();
-		CubeStage5 cube2 = new CubeStage5();
 		int mov_idx, mc, j, dist1, dist2;
 		int nDist = 0;
 		
-		copyTo (cube1);
-		dist1 = cube1.get_dist_edgcen();
+		int edge1 = edge;
+		int sym1 = sym;
+		int center1 = center;
+		dist1 = prune_table_edgcen.get_dist_packed(edge1*Constants.N_STAGE5_CENTERS+Tables.conjCenter5[center1][sym1]);
 
 		while ( true ) {
 			boolean noMoves=true;
 			for (mov_idx = 0; mov_idx < Constants.N_STAGE5_MOVES; ++mov_idx) {
-				cube1.copyTo (cube2);
-				cube2.do_move (mov_idx);
-				dist2 = cube2.get_dist_edgcen();
+				int center2 = Tables.moveCenter5[center1][mov_idx];
+				int edge2 = Tables.moveEdge5[edge1][Symmetry.moveConjugate5[mov_idx][sym1]];
+				int sym2 = Symmetry.symIdxCo4Multiply[sym1][edge2&0xFF];
+				edge2 >>= 8;
+				dist2 = prune_table_edgcen.get_dist_packed(edge2*Constants.N_STAGE5_CENTERS+Tables.conjCenter5[center2][sym2]);
 				if (((dist2+1) % 3) != dist1) continue;
-				cube2.copyTo (cube1);
-				nDist++;
+				center1 = center2;
+				edge1 = edge2;
+				sym1 = sym2;
 				dist1 = dist2;
+				nDist++;
 				noMoves=false;
 				break;
 			}
@@ -65,7 +69,7 @@ public final class CubeStage5 {
 				break;
 		}
 		/* Can be removed for speedup */
-		if( ( Tables.conjCenter5[cube1.center][cube1.sym] != 0 ) || ( cube1.edge != 0 ))
+		if( ( Tables.conjCenter5[center1][sym1] != 0 ) || ( edge1 != 0 ))
 			System.out.println("Wrong pruning distance for stage 5 edges+centers");
 		return nDist;
 	}
