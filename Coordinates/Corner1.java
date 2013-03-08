3e5241eaab7bcfb06ce4d5d1ae5dae02f4842897
Clément Gallet <clement.gallet@ens-lyon.org>
2013-03-08 19:18:52 +0100

    Override the init method of Corner1

--- Coordinates/Corner1.java
+++ Coordinates/Corner1.java
@@ -2,7 +2,9 @@ package cg.fivestage444.Coordinates;
 
 import cg.fivestage444.Cubies.CornerCubies;
 import cg.fivestage444.Cubies.Cubies;
+import cg.fivestage444.Moves;
 import cg.fivestage444.Stages.Stage1;
+import cg.fivestage444.Symmetry;
 
 public final class Corner1 extends RawCoord {
 
@@ -38,4 +40,27 @@ public final class Corner1 extends RawCoord {
 		}
 		return coord;
 	}
+
+	@Override
+	public void init(){
+		move = new short[N_COORD][N_MOVES];
+		conj = new short[N_COORD][N_SYM];
+
+		CornerCubies cube1 = new CornerCubies();
+		CornerCubies cube2 = new CornerCubies();
+
+		for (int u = 0; u < N_COORD; ++u) {
+			unpack(cube1, u);
+			for (int m = 0; m < N_MOVES; ++m) {
+				cube1.move (Moves.stage2moves[m], cube2);
+				move[u][m] = (short) pack(cube2);
+			}
+			for (int s = 0; s < N_SYM; ++s) {
+				cube1.rightMult (Symmetry.invSymIdx[s], cube2);
+				cube2.deMirror(); /* I need to override this method just to add this line... :( */
+				conj[u][s] = (short) pack(cube2);
+			}
+		}
+	}
+
 }
